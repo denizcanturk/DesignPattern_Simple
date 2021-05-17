77f484f57221164d22c170b2018f724c8164b2f0
denizcanturk <denizinciebru@gmail.com>
2021-05-17 20:08:09 +0300

    Abstruct Design Pattern Initial Commit

---
+++ AbstructDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstructDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
