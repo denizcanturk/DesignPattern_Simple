617d0f22fdcbc681d81bc538aaa644491bc693d3
denizcanturk <denizinciebru@gmail.com>
2021-05-17 20:21:21 +0300

    Copy of former Factory

---
+++ AbsFactory/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Factory Design Pattern World!");
+
+            Telefon s7 = TelefonFabrikası.getTelefon("S7", "3400", 6,8);
+
+            Telefon Note3 = TelefonFabrikası.getTelefon("Note3", "6555", 12,15);
+
+        }
+    }
+
+}
